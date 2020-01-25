6f049d7be49c63c843298344f03d1d60d1f46814
lukas <lukas.weidich@gmx.de>
2020-01-25 10:56:25 +0100

    everything is running on linux

--- React Native - The Practical Guide 2020/guess-a-number/App.js
+++ React Native - The Practical Guide 2020/guess-a-number/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Just a test to see if everything is working fine on linux</Text>
     </View>
   );
 }
