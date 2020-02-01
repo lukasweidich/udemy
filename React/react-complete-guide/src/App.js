9a5b6244045a2d8bf90e03a5abcedddc2c390636
lukas <lukas.weidich@gmx.de>
2020-02-01 18:27:40 +0100

    45

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from "./Person/Person"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>abc</code> and save to reload.
-        </p>
-      </div>
-    );
+const app = props => {
+
+  let [persons, setPersons] = new useState([{ name: "Lukas", age: 20 }, { name: "Hubert", age: 59 }]);
+
+  const switchNameHandler = () => {
+    setPersons([{ name: "Lukas Weidich", age: 20 }, { name: "Hubert Weidich", age: 59 }])
   }
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App!</h1>
+      <h1>This is really working!</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={persons[0].name} age={persons[0].age}></Person>
+      <Person name={persons[1].name} age={persons[1].age}></Person>
+    </div>
+  );
 }
 
-export default App;
+export default app;
