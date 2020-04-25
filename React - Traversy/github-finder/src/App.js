f1b1f92fc540f71c9e1260ec982568c89deec94d
lukas <lukas.weidich@gmx.de>
2020-04-26 00:40:31 +0200

    10

--- React - Traversy/github-finder/src/App.js
+++ React - Traversy/github-finder/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
-
+import Navbar from './components/layout/Navbar';
+/*  Function Based Component
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from React</h1>
     </div>
   );
+}
+*/
+
+/*  Class Based Component */
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Navbar />
+        <h1>Hello</h1>
+      </Fragment>
+    );
+  }
+  /* React.createElement('elementTag', {elementProp: 42, foo: "bar"}, 
+  React.createElement('h1', null, 'Hello from React' )); */
 }
 
 export default App;
