41a88c68daad26e6a23ae4bf06b8f0e1354da51c
lukas <lukas.weidich@gmx.de>
2020-06-22 13:59:44 +0200

    62

---
+++ Angular React Vue/vue/vue-framework-intro/src/components/Account.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h3>Account</h3>
+    <p>This is your account</p>
+  </div>
+</template>
---
+++ Angular React Vue/vue/vue-framework-intro/src/components/Info.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>This is some important information!</p>
+</template>
